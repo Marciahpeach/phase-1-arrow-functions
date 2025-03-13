25840e26e7d1cd92f798fe41425356b97d76641f
Marciah <marciah.machoka@student.moringaschool.com>
2025-03-14 01:49:55 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide =(p1,p2)=>{
+    console.log(`divide ${p1}`);
+    console.log(`divide ${p2}`)
+    return p1/p2
+}
+divide (2000,100)
+
+const square = (num) => num * num;
+console.log(square(5));
+console.log(square(10))
+
+
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+
+
